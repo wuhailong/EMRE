133a44ec3edfaf00f82e224d4dcaafdc4e3132ec
huzuohuyou <1255246777@qq.com>
2015-06-12 17:04:31 +0800

    1.0.0.1
    first commit
    完成winform 调用JavaScript操作。

---
+++ EMRE/MainForm.Designer.cs
@@ -0,0 +1,344 @@
+﻿namespace EMRE
+{
+    partial class EMRE
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.文件ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.打开ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.保存ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.另存为ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.关闭ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.内容ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.获取整个htmlToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.获取内容ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.写入内容ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.追加内容ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.获取纯文本ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.获取带格式文本ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.判断是否有内容ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.获取选中文本ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.编辑器ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.可以编辑ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.不可编辑ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.创建编辑器ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.删除编辑器ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.草稿箱ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.wb_editor = new System.Windows.Forms.WebBrowser();
+            this.button1 = new System.Windows.Forms.Button();
+            this.清空草稿箱ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.获取草稿箱内容ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.插入给定内容ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.文件ToolStripMenuItem,
+            this.内容ToolStripMenuItem,
+            this.编辑器ToolStripMenuItem,
+            this.草稿箱ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1008, 25);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 文件ToolStripMenuItem
+            // 
+            this.文件ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.打开ToolStripMenuItem,
+            this.保存ToolStripMenuItem,
+            this.另存为ToolStripMenuItem,
+            this.关闭ToolStripMenuItem});
+            this.文件ToolStripMenuItem.Name = "文件ToolStripMenuItem";
+            this.文件ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.文件ToolStripMenuItem.Text = "文件";
+            // 
+            // 打开ToolStripMenuItem
+            // 
+            this.打开ToolStripMenuItem.Name = "打开ToolStripMenuItem";
+            this.打开ToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.打开ToolStripMenuItem.Text = "打开";
+            // 
+            // 保存ToolStripMenuItem
+            // 
+            this.保存ToolStripMenuItem.Name = "保存ToolStripMenuItem";
+            this.保存ToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.保存ToolStripMenuItem.Text = "保存";
+            // 
+            // 另存为ToolStripMenuItem
+            // 
+            this.另存为ToolStripMenuItem.Name = "另存为ToolStripMenuItem";
+            this.另存为ToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.另存为ToolStripMenuItem.Text = "另存为";
+            // 
+            // 关闭ToolStripMenuItem
+            // 
+            this.关闭ToolStripMenuItem.Name = "关闭ToolStripMenuItem";
+            this.关闭ToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.关闭ToolStripMenuItem.Text = "关闭";
+            // 
+            // 内容ToolStripMenuItem
+            // 
+            this.内容ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.获取整个htmlToolStripMenuItem,
+            this.获取内容ToolStripMenuItem,
+            this.写入内容ToolStripMenuItem,
+            this.追加内容ToolStripMenuItem,
+            this.获取纯文本ToolStripMenuItem,
+            this.获取带格式文本ToolStripMenuItem,
+            this.判断是否有内容ToolStripMenuItem,
+            this.获取选中文本ToolStripMenuItem,
+            this.插入给定内容ToolStripMenuItem});
+            this.内容ToolStripMenuItem.Name = "内容ToolStripMenuItem";
+            this.内容ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.内容ToolStripMenuItem.Text = "内容";
+            // 
+            // 获取整个htmlToolStripMenuItem
+            // 
+            this.获取整个htmlToolStripMenuItem.Name = "获取整个htmlToolStripMenuItem";
+            this.获取整个htmlToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
+            this.获取整个htmlToolStripMenuItem.Text = "获取整个html";
+            this.获取整个htmlToolStripMenuItem.Click += new System.EventHandler(this.获取整个htmlToolStripMenuItem_Click);
+            // 
+            // 获取内容ToolStripMenuItem
+            // 
+            this.获取内容ToolStripMenuItem.Name = "获取内容ToolStripMenuItem";
+            this.获取内容ToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
+            this.获取内容ToolStripMenuItem.Text = "获取内容";
+            this.获取内容ToolStripMenuItem.Click += new System.EventHandler(this.获取内容ToolStripMenuItem_Click);
+            // 
+            // 写入内容ToolStripMenuItem
+            // 
+            this.写入内容ToolStripMenuItem.Name = "写入内容ToolStripMenuItem";
+            this.写入内容ToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
+            this.写入内容ToolStripMenuItem.Text = "写入内容";
+            this.写入内容ToolStripMenuItem.Click += new System.EventHandler(this.写入内容ToolStripMenuItem_Click);
+            // 
+            // 追加内容ToolStripMenuItem
+            // 
+            this.追加内容ToolStripMenuItem.Name = "追加内容ToolStripMenuItem";
+            this.追加内容ToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
+            this.追加内容ToolStripMenuItem.Text = "追加内容";
+            this.追加内容ToolStripMenuItem.Click += new System.EventHandler(this.追加内容ToolStripMenuItem_Click);
+            // 
+            // 获取纯文本ToolStripMenuItem
+            // 
+            this.获取纯文本ToolStripMenuItem.Name = "获取纯文本ToolStripMenuItem";
+            this.获取纯文本ToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
+            this.获取纯文本ToolStripMenuItem.Text = "获取纯文本";
+            this.获取纯文本ToolStripMenuItem.Click += new System.EventHandler(this.获取纯文本ToolStripMenuItem_Click);
+            // 
+            // 获取带格式文本ToolStripMenuItem
+            // 
+            this.获取带格式文本ToolStripMenuItem.Name = "获取带格式文本ToolStripMenuItem";
+            this.获取带格式文本ToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
+            this.获取带格式文本ToolStripMenuItem.Text = "获取带格式文本";
+            this.获取带格式文本ToolStripMenuItem.Click += new System.EventHandler(this.获取带格式文本ToolStripMenuItem_Click);
+            // 
+            // 判断是否有内容ToolStripMenuItem
+            // 
+            this.判断是否有内容ToolStripMenuItem.Name = "判断是否有内容ToolStripMenuItem";
+            this.判断是否有内容ToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
+            this.判断是否有内容ToolStripMenuItem.Text = "判断是否有内容";
+            this.判断是否有内容ToolStripMenuItem.Click += new System.EventHandler(this.判断是否有内容ToolStripMenuItem_Click);
+            // 
+            // 获取选中文本ToolStripMenuItem
+            // 
+            this.获取选中文本ToolStripMenuItem.Name = "获取选中文本ToolStripMenuItem";
+            this.获取选中文本ToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
+            this.获取选中文本ToolStripMenuItem.Text = "获取选中文本";
+            this.获取选中文本ToolStripMenuItem.Click += new System.EventHandler(this.获取选中文本ToolStripMenuItem_Click);
+            // 
+            // 编辑器ToolStripMenuItem
+            // 
+            this.编辑器ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.可以编辑ToolStripMenuItem,
+            this.不可编辑ToolStripMenuItem,
+            this.创建编辑器ToolStripMenuItem,
+            this.删除编辑器ToolStripMenuItem});
+            this.编辑器ToolStripMenuItem.Name = "编辑器ToolStripMenuItem";
+            this.编辑器ToolStripMenuItem.Size = new System.Drawing.Size(56, 21);
+            this.编辑器ToolStripMenuItem.Text = "编辑器";
+            // 
+            // 可以编辑ToolStripMenuItem
+            // 
+            this.可以编辑ToolStripMenuItem.Name = "可以编辑ToolStripMenuItem";
+            this.可以编辑ToolStripMenuItem.Size = new System.Drawing.Size(172, 22);
+            this.可以编辑ToolStripMenuItem.Text = "可以编辑";
+            this.可以编辑ToolStripMenuItem.Click += new System.EventHandler(this.可以编辑ToolStripMenuItem_Click);
+            // 
+            // 不可编辑ToolStripMenuItem
+            // 
+            this.不可编辑ToolStripMenuItem.Name = "不可编辑ToolStripMenuItem";
+            this.不可编辑ToolStripMenuItem.Size = new System.Drawing.Size(172, 22);
+            this.不可编辑ToolStripMenuItem.Text = "不可编辑";
+            this.不可编辑ToolStripMenuItem.Click += new System.EventHandler(this.不可编辑ToolStripMenuItem_Click);
+            // 
+            // 创建编辑器ToolStripMenuItem
+            // 
+            this.创建编辑器ToolStripMenuItem.Name = "创建编辑器ToolStripMenuItem";
+            this.创建编辑器ToolStripMenuItem.Size = new System.Drawing.Size(172, 22);
+            this.创建编辑器ToolStripMenuItem.Text = "创建编辑器";
+            this.创建编辑器ToolStripMenuItem.Click += new System.EventHandler(this.创建编辑器ToolStripMenuItem_Click);
+            // 
+            // 删除编辑器ToolStripMenuItem
+            // 
+            this.删除编辑器ToolStripMenuItem.Name = "删除编辑器ToolStripMenuItem";
+            this.删除编辑器ToolStripMenuItem.Size = new System.Drawing.Size(172, 22);
+            this.删除编辑器ToolStripMenuItem.Text = "删除编辑器";
+            this.删除编辑器ToolStripMenuItem.Click += new System.EventHandler(this.删除编辑器ToolStripMenuItem_Click);
+            // 
+            // 草稿箱ToolStripMenuItem
+            // 
+            this.草稿箱ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.清空草稿箱ToolStripMenuItem,
+            this.获取草稿箱内容ToolStripMenuItem});
+            this.草稿箱ToolStripMenuItem.Name = "草稿箱ToolStripMenuItem";
+            this.草稿箱ToolStripMenuItem.Size = new System.Drawing.Size(56, 21);
+            this.草稿箱ToolStripMenuItem.Text = "草稿箱";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 25);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.wb_editor);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.button1);
+            this.splitContainer1.Size = new System.Drawing.Size(1008, 705);
+            this.splitContainer1.SplitterDistance = 824;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // wb_editor
+            // 
+            this.wb_editor.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.wb_editor.Location = new System.Drawing.Point(0, 0);
+            this.wb_editor.MinimumSize = new System.Drawing.Size(20, 20);
+            this.wb_editor.Name = "wb_editor";
+            this.wb_editor.Size = new System.Drawing.Size(824, 705);
+            this.wb_editor.TabIndex = 0;
+            this.wb_editor.Url = new System.Uri("", System.UriKind.Relative);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(27, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // 清空草稿箱ToolStripMenuItem
+            // 
+            this.清空草稿箱ToolStripMenuItem.Name = "清空草稿箱ToolStripMenuItem";
+            this.清空草稿箱ToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
+            this.清空草稿箱ToolStripMenuItem.Text = "清空草稿箱";
+            this.清空草稿箱ToolStripMenuItem.Click += new System.EventHandler(this.清空草稿箱ToolStripMenuItem_Click);
+            // 
+            // 获取草稿箱内容ToolStripMenuItem
+            // 
+            this.获取草稿箱内容ToolStripMenuItem.Name = "获取草稿箱内容ToolStripMenuItem";
+            this.获取草稿箱内容ToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
+            this.获取草稿箱内容ToolStripMenuItem.Text = "获取草稿箱内容";
+            this.获取草稿箱内容ToolStripMenuItem.Click += new System.EventHandler(this.获取草稿箱内容ToolStripMenuItem_Click);
+            // 
+            // 插入给定内容ToolStripMenuItem
+            // 
+            this.插入给定内容ToolStripMenuItem.Name = "插入给定内容ToolStripMenuItem";
+            this.插入给定内容ToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
+            this.插入给定内容ToolStripMenuItem.Text = "插入给定内容";
+            this.插入给定内容ToolStripMenuItem.Click += new System.EventHandler(this.插入给定内容ToolStripMenuItem_Click);
+            // 
+            // EMRE
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1008, 730);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "EMRE";
+            this.Text = "EMRE";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 文件ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 打开ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 保存ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 另存为ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 关闭ToolStripMenuItem;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.WebBrowser wb_editor;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ToolStripMenuItem 内容ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 获取整个htmlToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 获取内容ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 写入内容ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 追加内容ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 获取纯文本ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 获取带格式文本ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 判断是否有内容ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 获取选中文本ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 编辑器ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 可以编辑ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 不可编辑ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 创建编辑器ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 删除编辑器ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 草稿箱ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 清空草稿箱ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 获取草稿箱内容ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 插入给定内容ToolStripMenuItem;
+    }
+}
+
